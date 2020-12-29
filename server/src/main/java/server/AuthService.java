5cd531b07e7ba45332003d3edd371524e38415ce
klotoz <ikskulfg@gmail.com>
2020-12-29 14:18:55 +0300

    Initial commit

---
+++ server/src/main/java/server/AuthService.java
@@ -0,0 +1,13 @@
+package server;
+
+public interface AuthService {
+
+    /**
+     * @return - nickname если пользоатель есть и null если пользователя нет
+     */
+    String getNicknameByLoginAndPassword(String login, String password);
+
+    boolean registration(String login, String password, String nickname);
+
+    boolean changeNick(String oldNickname, String newNickname);
+}
